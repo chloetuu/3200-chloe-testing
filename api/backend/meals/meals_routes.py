19645175b26c02a7b733069d728a07f5515c46e2
chloetuu <chloetu@yahoo.com>
2025-04-10 15:44:17 -0400

    make meals blueprint

---
+++ api/backend/meals/meals_routes.py
@@ -0,0 +1,28 @@
+########################################################
+# Sample users blueprint of endpoints
+# Remove this file if you are not using it in your project
+########################################################
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
+meals = Blueprint('meals', __name__ )
+
+@meals.route('/meals', methods=['GET'])
+def get_all_meals():
+    cursor = db.get_db().cursor()
+    the_query = '''
+    SELECT Username, FirstName, LastName, Region, ActivityLevel, Age, InclusionStatus, Bio
+    FROM Users 
+'''
+
+    cursor.execute(the_query)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
+    the_response.status_code = 200 
+    the_response.mimetype='application/json'
+    return the_response
