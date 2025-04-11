775c77ee2ede28cedb9b46a1c3652662b3471bd9
chloetuu <chloetu@yahoo.com>
2025-04-11 16:59:58 -0400

    second commit

--- api/backend/meals/meals_routes.py
+++ api/backend/meals/meals_routes.py
@@ -26,3 +26,23 @@ def get_all_meals():
     the_response.status_code = 200 
     the_response.mimetype='application/json'
     return the_response
+
+@meals.route('/meals', methods=['POST'])
+def add_meal():
+    current_app.logger.info('POST /meals route')
+    meal_info = request.json
+    username = meal_info['username']
+    meal_name = meal_info['meal_name']
+    calories = meal_info['calories']
+    meal_time = meal_info['meal_time']
+    notes = meal_info.get('notes', '')  
+
+    query = '''INSERT INTO Meals (Username, MealName, Calories, MealTime, Notes)
+               VALUES (%s, %s, %s, %s, %s)'''
+    data = (username, meal_name, calories, meal_time, notes)
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query, data)
+    db.get_db().commit()
+
+    return 'Meal added!'
