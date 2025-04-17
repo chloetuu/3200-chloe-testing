ee5f66b837d8345c2709fd9761aeeb4e799c3a58
PINKhedgehogs <melanieyu999@gmail.com>
2025-04-16 21:41:20 -0400

    Revert

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -11,7 +11,6 @@ from backend.error.error_routes import issues
 from backend.users.users_routes import users
 from backend.interactions.interaction_routes import interactions
 from backend.logs.log_routes import logs
-from backend.data_editing.data_editing_routes import data_editing
 
 import os
 from dotenv import load_dotenv
@@ -88,7 +87,6 @@ def create_app():
     app.register_blueprint(users,      url_prefix='/u')
     app.register_blueprint(interactions, url_prefix='/i')
     app.register_blueprint(logs,        url_prefix='/logs')
-    app.register_blueprint(data_editing, url_prefix='/d')
     
     
 
